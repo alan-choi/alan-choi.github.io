71a60afdeff673c6f87e22105af3a8180c5ae258
Alan Choi <alanchoi08@gmail.com>
2015-10-28 21:22:57 -0700

    add icons

--- Asteroids/lib/asteroid.js
+++ Asteroids/lib/asteroid.js
@@ -14,7 +14,7 @@ if (typeof Asteroids === 'undefined'){
 
   Asteroid.COLOR = '#696969';
   Asteroid.RADIUS = 60;
-  Asteroid.SPEED = 3;
+  Asteroid.SPEED = 2;
 
   Asteroids.Utils.inherits(Asteroid, Asteroids.MovingObject);
 
@@ -27,7 +27,7 @@ if (typeof Asteroids === 'undefined'){
 
   Asteroid.prototype.draw = function(ctx) {
     // var imageNum =(Math.floor(Math.random() * 5) + 1);
-    var imageNum =5;
+    var imageNum =3;
     var player = new Image();
     player.src = "images/"+imageNum+".png";
     ctx.drawImage(player, this.pos[0], this.pos[1]);
